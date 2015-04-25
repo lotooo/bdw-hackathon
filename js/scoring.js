2653726f6dedfdbb915de0b33f378dfd53b5f63b
Antoine Reversat <a.reversat@gmail.com>
2015-04-25 19:51:02 -0400

    Rework DataSource

--- js/scoring.js
+++ js/scoring.js
@@ -2,22 +2,22 @@ var initial_sources = {'Swiming pools': 'http://hackathon.cloudops.net/data.json
 var data_sources = []
 
 function DataSource(url, name) {
+    var curObj = this;
     this.name = name;
     this.url = url;
     this.data = d3.map();
     this.get_normalized_data = function(arr) {
-        var max = d3.max(this.data.values());
-        return this.data[arr]*100/max;
+        var max = d3.max(curObj.data.values());
+        return curObj.data[arr]*100/max;
     };
+    d3.json(curObj.url, function (error, json) {
+        if (error) return console.warn(error);
+        curObj.data = d3.map(json[0]);
+    });
 }
 
 function add_data_source(name, url) {
-    var ds = new DataSource(initial_sources[name], name);
-    d3.json(ds.url, function (error, json) {
-        if (error) return console.warn(error);
-        ds.data = d3.map(json[0]);
-    });
-    data_sources[data_sources.length] = ds;
+    data_sources[data_sources.length] = new DataSource(initial_sources[name], name);
 }
 
 function load_initial_data() {
