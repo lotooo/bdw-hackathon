af8d7c3b19e15a30900f20b38aac57d323e7cfb5
Antoine Reversat <a.reversat@gmail.com>
2015-04-25 18:52:48 -0400

    Add scoring.js

---
+++ js/scoring.js
@@ -0,0 +1,20 @@
+var data_sources = []
+
+function DataSource(url) {
+    this.url = url;
+    this.data = d3.map();
+    this.get_normalized_data = function() {
+        var ret = {};
+        var max = d3.max(data.values());
+
+        for (k in this.data)
+            ret[k] = this.data.k*100/max;
+
+        return ret;
+    };
+
+    d3.json(url, function (error, json) {
+        if (error) return console.warn(error);
+        this.data = d3.map(json);
+    });
+}
