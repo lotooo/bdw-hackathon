4283315d6cc5d81f276081448dbd38e70d32a305
Antoine Reversat <a.reversat@gmail.com>
2015-04-25 19:32:55 -0400

    Add a load_data function to initializer datasources

--- js/scoring.js
+++ js/scoring.js
@@ -1,3 +1,4 @@
+var initial_sources = {'Swiming pools': 'http://hackathon.cloudops.net/data.json'}
 var data_sources = []
 
 function DataSource(url, name) {
@@ -13,9 +14,15 @@ function DataSource(url, name) {
 
         return ret;
     };
+}
 
-    d3.json(url, function (error, json) {
-        if (error) return console.warn(error);
-        this.data = d3.map(json[0]);
-    });
+function load_data() {
+    for (name in initial_sources){
+        var ds = new DataSource(initial_sources[name], name);
+        d3.json(ds.url, function (error, json) {
+            if (error) return console.warn(error);
+            ds.data = d3.map(json[0]);
+        });
+        data_sources[data_sources.length] = ds;
+    }    
 }
