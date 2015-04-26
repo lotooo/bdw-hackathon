838cdb0831dbb0a9592cf4c8ec85504ed03dcc30
Antoine Reversat <a.reversat@gmail.com>
2015-04-25 20:38:27 -0400

    Iterate over length

--- js/hackathon.js
+++ js/hackathon.js
@@ -52,7 +52,7 @@ function makeMap(error, gjson_1) {
             html =  '<h4>' +  props.NOM + '</h4>';
             html += "<br /><span class='i_title'>Code</span> : " + props.ABREV;
             html += "<br /><span class='i_title'>Surface : </span>" + (props.AIRE / 1000000).toFixed(2) + " km<sup>2</sup>";
-            for (var i = 0; i < data_sources; i ++) {
+            for (var i = 0; i < data_sources.length; i ++) {
                 html += "<br /><span class='i_title'>"+data_source[i].name+": </span>" + data_source.data.get(props.ABREV);
             }
         }
